126321f372040e3da6b50cfe443e6413b0cd0018
Ivan Mickovski <ivan_mickovski@hotmail.com>
2020-08-17 16:49:14 +0200

    Search functionality working

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
-// import { STATES } from 'mongoose';
 
 function App() {
   const [name, setName] = useState('');
@@ -9,11 +8,9 @@ function App() {
   const [location, setLocation] = useState('');
   const [public_repos, setPublicRepos] = useState('');
   const [avatar_url, setAvatar] = useState('');
-  // This is the Repos Array from the User
+  // Repos Array from the User
   const [repos, setGithubRepos] = useState([]);
-
-
-
+  // Search state for the search bar
   const [search, setSearch] = useState('');
 
   // Implementing the Data into the states
@@ -48,9 +45,24 @@ function App() {
       });
   }, []);
 
+  const handleNameChange = event => {
+    // console.log(event.target.value)
+    setSearch(event.target.value)
+  }
+
+  // Filtering the Repos after search
+  const filterRepos = repos.filter(repo => {
+    // console.log(repo)
+    if(handleNameChange) {
+      return (repo.name.toLowerCase().includes(search.toLowerCase()));
+    }
+  })
+
   return (
+    
     <div className="App">
       <h1>Github Search </h1>
+
       <form>
         <label htmlFor='repos'> Search Repos</label>
         <input
@@ -59,24 +71,26 @@ function App() {
           id='repos'
           placeholder='Find a repository'
           value={search}
-        // onChange={handleNameChange}
+          onChange={handleNameChange}
         />
       </form>
 
+      {/* Rendering the list of the repos */}
+      <div>
+        {filterRepos.map(e =>
+          <ul key={e.id}>
+            <li>{e.name}</li>
+          </ul>
+        )}
+      </div>
+
+      {/* Rendering The User Data */}
       <div>
         <h1><img src={avatar_url} /></h1>
         <h1>{name}</h1>
         <h1>{login}</h1>
         <h1>{location}</h1>
         <h1>Repos: {public_repos}</h1>
-      </div>
-
-      <div>
-        {repos.map(e =>
-          <ul key={e.id}>
-            <li>{e.name}</li>
-          </ul>
-        )}
       </div>
 
     </div>
